a243c124ca57f6673ac44c90fe299a5f17893bf1
Timilehin Okunola <okunolatimilehin4123@gmail.com>
2024-06-19 17:15:39 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import orchard from './Json/orchard.json';
+import sapling from './Json/sapling.json';
+import sprout from './Json/sprout.json'
 
 function App() {
+
+  console.log(orchard, sapling, sprout)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
